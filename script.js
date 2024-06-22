6e4f09136c61176aa045cdfb0789ead74a9670b5
Teremki16 <teremki16robocode@gmail.com>
2024-06-22 19:21:37 +0300

    Co-authored-by: Teremki16 <Teremki16@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let url = "http://api.weatherapi.com/v1/current.json?key=0fc0fa0b48294f9fb74151643240106"
+let url = "http://api.weatherapi.com/v1/forecast.json?key=0fc0fa0b48294f9fb74151643240106&q=Kyiv&days=10&aqi=no&alerts=no"
 let xhr = new XMLHttpRequest()
 xhr.open("GET", url + "&q=London&aqi=no")
 xhr.send()
@@ -41,16 +41,30 @@ document.querySelector("#windUnit").addEventListener("change", function (event) 
 
 function render(data) {
     document.querySelector(".weather img").setAttribute("src", data.current.condition.icon)
-    document.querySelector(".temperature").innerHTML = (t == "celsius" ? data.current.temp_c : t == "fahrenheit" ? data.current.temp_f : data.current.temp_c + 273.15) + "<span>°C</span><i class='bx bx-thermometer'></i>"
+    document.querySelector(".temperature").innerHTML = (t == "celsius" ? data.current.temp_c : t == "fahrenheit" ? data.current.temp_f : data.current.temp_c + 273.15) + `<span>${(t == "celsius" ? "℃" : t == "fahrenheit" ? "℉" : "°K")}</span><i class='bx bx-thermometer'></i>`
     document.querySelector(".info-wind span").innerHTML = w == "kph" ? (data.current.wind_kph + "k/h") : (data.current.wind_mph + "m/h")
     document.querySelector(".weather .description").innerHTML = data.current.condition.text
     document.querySelector(".info-humidity span").innerHTML = data.current.humidity + "%"
+
+    document.querySelector(".days").innerHTML = ""
+    data.forecast.forecastday.forEach((day) => {
+        document.querySelector(".days").innerHTML += `
+ 
+ <div class="item">
+            <div class="data">${new Date(day.date).toLocaleDateString()}</div>
+            <div class="icon"><img src="${day.day.condition.icon}"</div>
+            <div class="text">${day.day.condition.text}</div>
+            <div class="temp">${day.day.avgtemp_c}℃</div>
+            <div class="rain">Chance of rain: ${day.day.daily_chance_of_rain}%</div>
+        </div>
+        `
+    })
 }
 
 document.querySelector("#locationInput").addEventListener("input", function(event){
     let text = event.target.value
     let axhr = new XMLHttpRequest()
-    axhr.open("GET", "http://api.weatherapi.com/v1/search.json?key=0fc0fa0b48294f9fb74151643240106&q=" + text)
+    axhr.open("GET", `http://api.weatherapi.com/v1/forecast.json?key=0fc0fa0b48294f9fb74151643240106&q=${text}&days=10&aqi=no&alerts=no`)
     axhr.onload = function(){
         let arr = JSON.parse(axhr.response)
         document.querySelector("#list").innerHTML=""
@@ -59,5 +73,9 @@ document.querySelector("#locationInput").addEventListener("input", function(even
         })
     }
     axhr.send()
+    
+
 
 })
+
+
